1256b9b1c12b39dc8e5ad46f4a3aa1d18d9d882f
Vitaliy Dybov <VitaliyDybov@gmail.com>
2017-09-22 16:33:42 +0300

    after refractoring. add comments

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,82 +1,71 @@
 class TicTacToe {    
     constructor() {
-        this.turn = 0;
+        this.turnCount = 0;
         this.firstSymbol = 'x';      
-        this.secondSymbol = 'o';      
-        this.marksStorage = [[],[],[]];//this.init(3);
-        this.winner = null;
-        this.resultArr = [];  
-        this.currentSymbol = this.firstSymbol;      
+        this.secondSymbol = 'o';    
+        this.currentSymbol = this.firstSymbol;  
+        this.marksStorage = [[],[],[]];        
+        this.resultArr = []; 
+        this.winner = null;      
     }
-
-    // init(num){
-    //     for(let i = 0; i < num; i++ ){ 
-    //         this.marksStorage = []; 
-    //         for(let j = 0; j< num; j++){              
-    //             this.marksStorage.push(false);
-    //         } 
-    //     }
-    //     return  this.marksStorage;
-    // }
-
+    /**
+     * depending on the turnCount return certain symbol
+     */
     getCurrentPlayerSymbol() {
-        return this.currentSymbol = (this.turn%2 === 0) ?  this.firstSymbol : this.secondSymbol;
-        // if (this.turn%2 === 0)
-        //     return this.firstSymbol;
-        // else
-        //     return this.secondSymbol;
+        return this.currentSymbol = (this.turnCount%2 === 0) ?  this.firstSymbol : this.secondSymbol;
     }
 
+    /**
+     * fill the cell with currentsymbol and change turnCounter, if cell is empty. 
+     * else ignore this turn
+     * @param {*int} rowIndex 
+     * @param {*int} columnIndex 
+     */
     nextTurn(rowIndex, columnIndex) {  
-        
+                
         if (this.marksStorage[rowIndex][columnIndex]) {
              return false;
         } else {
             this.marksStorage[rowIndex][columnIndex] = this.getCurrentPlayerSymbol();
-            this.turn ++;
+            this.turnCount ++;
         }
-        this.resultArr = this.transformToResultsArr(this.marksStorage);
-
-       // console.log( this.marksStorage);        
+        this.resultArr = this.transformToResultsArr(this.marksStorage);  
     }
 
-    isFinished() {
-        
-     
-        //console.log(this.marksStorage);
-        // console.log(this.resultArr.some(this.checkWin) );
-        // console.log(this.resultArr.every(this.checkNoTurns));
-        // console.log(this.resultArr.every(this.checkDraw));
+    /**
+     * check all variants of finish the game
+     */
+    isFinished() {  
        return  !!this.noMoreTurns() ||  !!this.getWinner() || !!this.isDraw();
-        // return (this.resultArr.some(this.checkWin) 
-        //         || this.resultArr.every(this.checkNoTurns) 
-        //         || this.resultArr.every(this.checkDraw));
-
-            
-       
     }
 
+    /**
+     * c check all symbols in each full line to be equal firstSymbol or secondSymbol
+     * and change currentSymbol accordingly
+     */
     getWinner() {
-       // console.log(this.resultArr); 
+
         for(let i = 0; i < this.resultArr.length; i++){
-           //console.log(this.resultArr[i].join('').length );
+
             if(this.resultArr[i].join('').length === 3){
-                if(this.resultArr[i].every((x) => x === 'o')){
-                    this.winner = 'o';   
-                    return 'o';    
-                }  if(this.resultArr[i].every((x) => x === 'x')){
-                    this.winner = 'x'; 
-                    return 'x';
-                } 
+                if(this.resultArr[i].every((x) => x === 'o'))
+                   return this.winner =  this.secondSymbol;        
+                             
+                if(this.resultArr[i].every((x) => x === 'x'))
+                   return this.winner =  this.firstSymbol;
             } 
         }        
         return this.winner;
     }
 
+    /**
+     * check if every line is full
+     */
     noMoreTurns() {
-        return this.marksStorage.every(this.checkNoTurns);
+        return this.marksStorage.every((x)=> x.join('').length === 3);
     }
-
+    
+    //simple check of winner or fullness of marksStorage cells
     isDraw() {     
         if(this.getWinner())
             return false;        
@@ -85,45 +74,23 @@ class TicTacToe {
         return false;
    
     }
-
+    /**
+     * return certain cell from marks storage of exists, else null
+     * @param {*int} rowIndex 
+     * @param {*int} colIndex 
+     */
     getFieldValue(rowIndex, colIndex) {
         return this.marksStorage[rowIndex][colIndex] || null;
     }
 
-    checkNoTurns(element, index, array) {
-        return (element.join('').length === 3)
-        // if(element.length === 2){
-        //     return fasle
-        // }
-       // return element.every((x) => !!x);
-        // array.length === 3;
-        // array.every((x) => x !== undefined);            
-     }
-
-    // checkDraw(element, index, array){
-    //     //  let tempString = array.join(''); 
-
-    //     // return (tempString.length === 2 && tempString[0] !== tempString[1]);
-
-    //  }
-     
-    // checkWin(element, index, array) {
-    //     if(element.every((x) => x === 'o')){
-    //         Twinner = 'o';
-    //         return true;             
-    //     } else if(element.every((x) => x === 'x')){
-    //         Twinner = 'x';
-    //         return true;
-             
-    //     } else {                       
-    //         return false;
-    //     }
-    // }
-
+    /**
+     * method for transformation marksStorage[] into array of lines variants for easy scan and check
+     * @param {*string[[]]} array 
+     */
     transformToResultsArr(array) {        
         let transformedArr=[],
             tempArr=[];
-
+        //push all colls from array into transformedArr using tempArray
         for(let i = 0; i < array.length; i++ ){ 
             tempArr = []; 
             for(let j = 0; j< array.length; j++){              
@@ -131,17 +98,21 @@ class TicTacToe {
             } 
             transformedArr.push(tempArr);
         }
-        tempArr = [];
+         //push all rows from array into transformedArr 
+         //push diagonal(left-top-> right-bottom) from array into transformedArr using tempArray
+         tempArr = [];
         for(let i = 0; i < array.length; i++ ){           
             transformedArr.push(array[i])
             tempArr.push(array[i][i]); 
         }
         transformedArr.push(tempArr);
+         //push diagonal(left-bottom-> right-top) from array into transformedArr using tempArray
         tempArr = [];
         for(let i = array.length-1, j = 0; i >= 0; i--, j++ ){  
             tempArr.push(array[i][j]); 
         }
         transformedArr.push(tempArr); 
+
         return transformedArr;
     }
 }
